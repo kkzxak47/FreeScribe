7348ad4db6242737346c37831a986fd809e202fe
Xun Zhong <xzhong1794@conestogac.on.ca>
2025-01-29 15:25:49 -0500

    adopt log format suggestion

--- src/FreeScribe.client/client.py
+++ src/FreeScribe.client/client.py
@@ -59,7 +59,7 @@ else:
 
 logging.basicConfig(
     level=LOG_LEVEL,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format='%(asctime)s - %(threadName)s - %(name)s - %(levelname)s - %(message)s'
 )
 
 
