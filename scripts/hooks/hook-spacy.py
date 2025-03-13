22414a71cff83d7cb8f583b59ab660c1b452d629
Xun Zhong <victor.xunzhong@gmail.com>
2025-03-13 12:32:00 -0400

    fix: include init file

--- scripts/hooks/hook-spacy.py
+++ scripts/hooks/hook-spacy.py
@@ -10,7 +10,13 @@ hiddenimports = collect_submodules('spacy')
 # Add the en_core_web_md model data and its metadata
 model_data = collect_data_files('en_core_web_md')
 model_dist_info = collect_data_files('en_core_web_md-3.7.1.dist-info')
-datas += model_data + model_dist_info
+
+# Explicitly include __init__.py files
+init_files = [
+    ('en_core_web_md/__init__.py', 'en_core_web_md'),
+]
+
+datas += model_data + model_dist_info + init_files
 
 # Add specific hidden imports for the model
 hiddenimports += [
