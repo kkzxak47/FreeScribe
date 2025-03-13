26a4065a47466f07ef0099e7fc57c4dc882a724f
Xun Zhong <victor.xunzhong@gmail.com>
2025-03-13 13:10:26 -0400

    include spacy

--- scripts/hooks/hook-spacy.py
+++ scripts/hooks/hook-spacy.py
@@ -1,4 +1,6 @@
 from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+import os
+import spacy
 
 # Collect all spaCy data files
 datas = collect_data_files('spacy')
@@ -8,10 +10,12 @@ hiddenimports = collect_submodules('spacy')
 
 # Add the en_core_web_md model data and its metadata
 model_data = collect_data_files('en_core_web_md')
+model_dist_info = collect_data_files('en_core_web_md-3.7.1.dist-info')
 
-datas += model_data
-
-# Add specific hidden imports for the model
+# Add all necessary dependencies
 hiddenimports += [
     'en_core_web_md',
-] 
+    'spacy',
+]
+
+datas += model_data + model_dist_info 
