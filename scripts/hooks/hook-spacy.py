304ea0b0d69cd73aa367e312bc63ef75f9359327
Xun Zhong <victor.xunzhong@gmail.com>
2025-03-13 12:05:53 -0400

    add dist-info in hook-spacy

--- scripts/hooks/hook-spacy.py
+++ scripts/hooks/hook-spacy.py
@@ -1,4 +1,5 @@
 from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+import os
 
 # Collect all spaCy data files
 datas = collect_data_files('spacy')
@@ -6,5 +7,31 @@ datas = collect_data_files('spacy')
 # Collect all spaCy submodules
 hiddenimports = collect_submodules('spacy')
 
-# Add the en_core_web_md model data
-datas += collect_data_files('en_core_web_md') 
+# Add the en_core_web_md model data and its metadata
+model_data = collect_data_files('en_core_web_md')
+model_dist_info = collect_data_files('en_core_web_md-3.7.1.dist-info')
+datas += model_data + model_dist_info
+
+# Add specific hidden imports for the model
+hiddenimports += [
+    'en_core_web_md',
+    'en_core_web_md.language',
+    'en_core_web_md.language.lex_attrs',
+    'en_core_web_md.language.syntax_iterators',
+    'en_core_web_md.language.tag_map',
+    'en_core_web_md.language.tokenizer_exceptions',
+    'en_core_web_md.language.wordnet',
+    'en_core_web_md.language.lemmatizer',
+    'en_core_web_md.language.stop_words',
+    'en_core_web_md.language.punctuation',
+    'en_core_web_md.language.char_classes',
+    'en_core_web_md.language.lex_attrs',
+    'en_core_web_md.language.syntax_iterators',
+    'en_core_web_md.language.tag_map',
+    'en_core_web_md.language.tokenizer_exceptions',
+    'en_core_web_md.language.wordnet',
+    'en_core_web_md.language.lemmatizer',
+    'en_core_web_md.language.stop_words',
+    'en_core_web_md.language.punctuation',
+    'en_core_web_md.language.char_classes',
+] 
